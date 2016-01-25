c3a118d51030d73fec6989a5328be6a81a0b9dce
Courtney McRae <courtneymcrae9@gmail.com>
2016-01-25 12:20:36 -0800

    adds middleware and dependencies

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-var express 	= require('express'),
-	path 		= require('path'),
-	app 		= express(),
-	morgan		= require('morgan'),
-	bodyParser 	= require('body-parser'),
-	mongoose 	= require('mongoose'),
-	port 		= process.env.PORT || 3000
+var express      = require('express');
+var app          = express();
+var mongoose     = require('mongoose');
+var passport     = require('passport');
+var flash        = require('connect-flash');
+var ejsLayouts   = require("express-ejs-layouts");
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+mongoose.connect('mongodb://localhost/local-authentication-with-passport'); 
 
-app.set( 'views', path.join( __dirname, 'views' ) )
-app.engine( 'ejs', require( 'ejs' ).renderFile )
-app.set( 'view engine', 'ejs' )
+app.use(morgan('dev')); 
+app.use(cookieParser());
+app.use(bodyParser()); 
 
-app.listen(port) 
-console.log('server running on port' + port)
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.set("views","./views");
+app.use(express.static(__dirname + '/public'));
+
+app.use(session({ secret: 'WDI-GENERAL-ASSEMBLY-EXPRESS' })); 
+app.use(passport.initialize());
+app.use(passport.session()); 
+app.use(flash());
+
+require('./config/passport')(passport);
+
+// This middleware will allow us to use the current user in the layout
+app.use(function (req, res, next) {
+  global.user = req.user;
+  next()
+});
+
+var routes = require('./config/routes');
+app.use(routes);
+
+app.listen(3000);
--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,28 @@
 var User = require('../models/user');
 
+   function(req, email, password, callback) {
+    process.nextTick(function() {
 
+      // Find a user with this e-mail
+      User.findOne({ 'local.email' :  email }, function(err, user) {
+        if (err) return callback(err);
 
+        // If there already is a user with this email 
+        if (user) {
+          return callback(null, false, req.flash('signupMessage', 'This email is already used.'));
+        } else {
+        // There is no email registered with this email
 
+          // Create a new user
+          var newUser            = new User();
+          newUser.local.email    = email;
+          newUser.local.password = newUser.encrypt(password);
+
+          newUser.save(function(err) {
+            if (err) throw err;
+            return callback(null, newUser);
+          });
+        }
+      });
+    });
+  }));
