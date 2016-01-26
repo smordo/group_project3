ed6c9278e7507f6a22fa4b4fed12e49e5dc276af
Miwa Tachibana <tachibam@gmail.com>
2016-01-25 21:47:28 -0800

    fixes typos in restaurantRoutes

--- config/routes/restaurantRoutes.js
+++ config/routes/restaurantRoutes.js
@@ -1,16 +1,16 @@
 var 	express    			  = require('express'),
-		router 	   			  = express.Router(),
-		bodyParser 			  = require('body-parser'),
-		restaurantsController = require('../../controllers/restaurant.js')
+	router 	   			  = express.Router(),
+	bodyParser 			  = require('body-parser'),
+	restaurantsController 		  = require('../../controllers/restaurant.js')
 
 router.route('/')
-	.get(restaurantsController.index);
+	.get(restaurantsController.index)
 
 router.route('/:id')
-	.get(restaurantsController.show);
-	.post(restaurantsController.create);
-	.patch(restaurantsController.update);
-	.delete(restaurantsController.delete);
+	.get(restaurantsController.show)
+	.post(restaurantsController.create)
+	.patch(restaurantsController.update)
+	.delete(restaurantsController.delete)
 
 
 module.exports = router
