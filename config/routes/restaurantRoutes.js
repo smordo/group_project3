8b99fa6d3ef50f5a9a5b1cdefb95bf68923e49e2
Courtney McRae <courtneymcrae9@gmail.com>
2016-01-25 17:42:21 -0800

    updates restauraunt routes

--- config/routes/restaurantRoutes.js
+++ config/routes/restaurantRoutes.js
@@ -6,9 +6,11 @@ var 	express    			  = require('express'),
 router.route('/')
 	.get(restaurantsController.index);
 
-
-
+router.route('/:id')
+	.get(restaurantsController.show);
+	.post(restaurantsController.create);
+	.patch(restaurantsController.update);
+	.delete(restaurantsController.delete);
 
 
 module.exports = router
-
