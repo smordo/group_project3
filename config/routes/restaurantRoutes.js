93dbaf2cf28582940d64e5fad666e0082805e96a
Shon Mordo <s.mordo@gmail.com>
2016-01-27 11:03:34 -0800

    Configures controller and view for new restaurant action

--- config/routes/restaurantRoutes.js
+++ config/routes/restaurantRoutes.js
@@ -1,16 +1,17 @@
 var 	express    			  = require('express'),
-	router 	   			  = express.Router(),
-	restaurantsController 		  = require('../../controllers/restaurants')
+		router 	   			  = express.Router(),
+		restaurantsController = require('../../controllers/restaurants')
 
 router.route('/')
 	.get(restaurantsController.index)
+
+router.route('/new')
+	.get(restaurantsController.newRestaurant)
 	.post(restaurantsController.create)
 
 router.route('/:name')
 	.get(restaurantsController.show)
-	.post(restaurantsController.create)
 	.patch(restaurantsController.update)
 	.delete(restaurantsController.remove)
-
 
 module.exports = router
