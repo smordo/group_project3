095f2465a289b31b2cb3c83b00a6be6139fbbf9a
Miwa Tachibana <tachibam@gmail.com>
2016-01-28 12:38:09 -0800

    changes userRoutes from ('/:firstName') to ('/:id')

--- config/routes/userRoutes.js
+++ config/routes/userRoutes.js
@@ -1,18 +1,20 @@
-var 	express    			  = require('express'),
-	router 	   			  = express.Router(),
-	bodyParser 			  = require('body-parser'),
-	methodOverride 		  = require('method-override'),
-	passport 	          		  = require('passport'),
-	usersController 	 	  = require('../../controllers/users')
+var express = require('express');
+var router = express.Router();
+// Parses information from POST
+var bodyParser = require('body-parser');
+// Used to manipulate POST methods
+var methodOverride = require('method-override');
+var passport = require("passport");
+var usersController = require('../../controllers/users');
 
 router.route('/')
 	.get(usersController.getIndex);
 
-router.route('/:firstName')
+
+router.route('/:id')
 	.get(usersController.getUser)
 	.patch(usersController.editUser)
 	.delete(usersController.destroyUser);
-
 
 
 module.exports = router
